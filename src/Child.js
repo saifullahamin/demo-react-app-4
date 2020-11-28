d13d8ebf91cad9dc17b9072c055d614a8d3e16a0
Saifullah Amin <saifullahaminkhan@gmail.com>
2020-11-28 22:52:22 +0500

    Setup the boiler plate for this project

---
+++ src/Child.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Child() {
+    return(
+        <div>
+            <h1>Child Component</h1>
+        </div>
+    );
+}
+export default Child;
---
+++ src/Parent.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Child from "./Child";
+
+function Parent() {
+    return(
+        <div>
+            <h1>Parent Component</h1>
+            <hr />
+            <Child />
+        </div>
+    );
+}
+export default Parent;
